7d63d17b1fa789230111800e5b164302c0e08e65
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2026-01-24 16:35:24 +0400

    1929 py

---
+++ py/#1929.py
@@ -0,0 +1,10 @@
+# 1929. Concatenation of Array
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
+
+# I haven't list here but in leetcode you can try it directly.
+# https://leetcode.com/problems/concatenation-of-array
+
+# that wa so so easy, but i was trying it with loop first and then i realized its just simple as that🤧🥲
