f88b1e65a35f4b4cd3aacc0853a24510077ef39d
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2026-02-07 13:39:13 +0400

    1929 js

---
+++ js/#1929.js
@@ -0,0 +1,17 @@
+// 1929. Concatenation of Array
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+    let sum = nums.concat(nums);
+    return sum
+};
+
+// I haven't list here but in leetcode you can try it directly.
+// https://leetcode.com/problems/concatenation-of-array
+
+// i can say that im really dumb...how i forget js ....im really dumbbbbbb😭😭😭  okay i dont really know that about concat but i was always forgeting write vars🥲 i think python wasnt good choice after i learnt little js
+
+//i submited it and now i noticed that i can write it with push too....i need learn js from scratch again i think...i was really bad at js before but now i think i am worst than before😭😭😭 and also my copilot knows what i want to write....it knows that im not good at it
