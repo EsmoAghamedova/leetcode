1a16adc3ac97d765a8c09c8ab6dca5dfe15de54d
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2026-04-10 19:11:09 +0400

    #9.py

---
+++ py/#9.py
@@ -0,0 +1,10 @@
+# 9. Palindrome Number
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+
+# https://leetcode.com/problems/palindrome-number/
